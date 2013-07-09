cefa952a86c9622a2e0038dc40c4e95458172833
Cedric Villa <cedric.villa@gmail.com>
2013-07-09 15:19:53 +0000

    add master config

--- manifests/tools/set_config.pp
+++ manifests/tools/set_config.pp
@@ -3,13 +3,22 @@ define mv_postfix::tools::set_config($config_file = "${mv_postfix::params::confi
 	require('mv_postfix')
 	require('mv_postfix::packages')
 	
-    file { "${config_file}":
-      content => template('mv_postfix/main.cf.erb'),
-      ensure => file,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '644',
-	  notify => Service['mv_postfix'],
-      require => Package['mv_postfix'],
+	file { "${config_file}":
+		ensure 	=> file,
+		content => template('mv_postfix/main.cf.erb'),
+		owner   => 'root',
+		group   => 'root',
+		mode    => '644',
+		notify 	=> Service['mv_postfix'],
+		require => Package['mv_postfix'],
    }
+ 	file { "${mv_postfix::params::config_path}/master.cf":
+		ensure 	=> file,
+ 		content => template('mv_postfix/master.cf.erb'),
+ 		owner   => 'root',
+ 		group   => 'root',
+ 		mode    => '644',
+ 		notify 	=> Service['mv_postfix'],
+ 		require => Package['mv_postfix'],
+    }
 }
