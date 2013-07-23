9c2d6a4cead8e8c1a7ee88a6deaf60c4b4863ff2
Cedric Villa <cedric.villa@gmail.com>
2013-07-23 09:10:59 +0000

    add certs parameters + hostname

--- manifests/tools/set_config.pp
+++ manifests/tools/set_config.pp
@@ -1,4 +1,11 @@
-define mv_postfix::tools::set_config($config_file = "${mv_postfix::params::config_path}/main.cf", $config = {}) {
+define mv_postfix::tools::set_config(
+	$config_file = "${mv_postfix::params::config_path}/main.cf", 
+	$config = {
+		smtpd_tls_cert_file => "/etc/ssl/certs/ssl-cert-snakeoil.pem",
+		smtpd_tls_key_file 	=> "/etc/ssl/private/ssl-cert-snakeoil.key",
+	},
+)
+{
 	
 	require('mv_postfix')
 	require('mv_postfix::packages')
